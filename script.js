39ebf38e8f2a83ec007bda0865b7596279f7c0d7
nicha.satt <nicha.satt@gmail.com>
2022-05-19 12:03:45 +0700

    dbl change work (hard code)

--- script.js
+++ script.js
@@ -1,13 +1,52 @@
-document.getElementById('no').addEventListener('mousemove', function(e) {
-    console.log('x = ', e.screenX, " y = ",  e.screenY)
+document.getElementById('ok').addEventListener('click', function (e) {
+    let noElement = document.getElementById('no')
+    if(noElement.classList.contains('toggleOn')) {
+        noElement.classList.replace('toggleOn', 'toggleOff')
+    } else if(noElement.classList.contains('toggleOff')) {
+        noElement.classList.replace('toggleOff', 'toggleOn')
+    } else {
+        noElement.classList.add('toggleOn')
+    }
+
+    console.log(document.getElementById('no'))
 })
 
-let eleS = document.getElementsByClassName('flex-item')
-for (let ele of eleS) {
-    ele.addEventListener('mousemove', function(e) {
-        console.log('x = ', e.screenX, " y = ",  e.screenY)
-    })
-    ele.addEventListener('click', function(e) {
-        alert(ele.innerText)
-    })
-}
+/*let borderThick = 2
+document.getElementById('cancel').style.border = `${borderThick}px solid white`
+
+let cancelElement = document.getElementById('cancel')
+cancelElement.addEventListener('mousemove', function(e) {
+    cancelElement.innerHTML = `
+        cancle
+    `
+})*/
+
+
+const okElement = document.getElementById('ok')
+const cancelElement = document.getElementById('cancel')
+const noElement = document.getElementById('no')
+
+let margin = 20
+okElement.addEventListener('click', function(e) {
+    let marginSize = `20px ${margin}px`
+    cancelElement.style.margin = marginSize
+    noElement.style.margin = marginSize
+    margin+=5
+})
+
+//let words = ['ok', 'วิชานี้', 'ง่าย', 'จริงๆนะ']
+//showIndex = 0;
+noElement.addEventListener('dblclick', function() {
+    //showIndex++
+    if(okElement.textContent.includes('ok')) {
+        okElement.textContent = okElement.textContent.replace('ok', 'วิชานี้')
+    } else if(okElement.textContent.includes('วิชานี้')) {
+        okElement.textContent = okElement.textContent.replace('วิชานี้', 'ง่าย')
+    } else if(okElement.textContent.includes('ง่าย')) {
+        okElement.textContent = okElement.textContent.replace('ง่าย', 'จริงๆนะ')
+    } else {
+        okElement.textContent = 'ok'
+    }
+
+    console.log(okElement)
+})
