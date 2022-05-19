3ce502f3e25577008c4121208a261f4032363072
nicha.satt <nicha.satt@gmail.com>
2022-05-19 14:40:33 +0700

    dynamic dom flex-item

--- script.js
+++ script.js
@@ -2,17 +2,21 @@ const okElement = document.getElementById('ok')
 const cancelElement = document.getElementById('cancel')
 const noElement = document.getElementById('no')
 const textElement = document.getElementById('text')
-
 const outputContainer = document.getElementById('output-container')
 
-let text = ''
 
+let text = ''
 let elems = document.getElementsByClassName('flex-item')
 for (let elem of elems) {
     elem.addEventListener('click', function(e) {
         e.stopPropagation()
-        display(keyword[elem.id])
+        //display(keyword[elem.id])
+        addNode();
     })
+}
+function display(name) {
+    text = text + name +'\n'
+    textElement.innerText = text;
 }
 
 let keyword = {
@@ -21,15 +25,23 @@ let keyword = {
     'no' : 'No'
 }
 
-function onTextClicked(e) {
+/*function onTextClicked(e) {
     text = ''
     textElement.innerText = text
 }
-textElement.onclick = onTextClicked
+textElement.onclick = onTextClicked*/
 
-function display(name) {
-    text = text + name +'\n'
-    textElement.innerText = text;
+
+
+let counter = 0
+
+function addNode() {
+    newNode = document.createElement('div')
+    newNode.classList.add('flex-item')
+    newNode.setAttribute('id', counter)
+    newNode.innerText = counter
+    counter = counter +1 
+    newNode.innerText = counter
+    outputContainer.appendChild(newNode)
 }
-
 
